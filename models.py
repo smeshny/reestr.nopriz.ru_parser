0e4ea46c8fc7994c70dbab9d8a71321f5f135bd1
smeshny <smeshny@gmail.com>
2018-09-09 22:42:16 +0300

    Created get_companies_short_links() and added Companies.updated_at

--- models.py
+++ models.py
@@ -1,9 +1,11 @@
+import datetime
+
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
 
 
-engine = create_engine('sqlite:///database.sqlite', echo=True)
+engine = create_engine('sqlite:///database.sqlite', echo=False)
 
 Base = declarative_base()
 
@@ -19,10 +21,11 @@ class Companies(Base):
     company_link = Column(String, nullable=True)
     sro_belongs = Column(String, nullable=True)
     member_status = Column(String, nullable=True)
+    updated_at = Column(Date, onupdate=datetime.datetime.now())
 
     def __init__(self, company_name, company_inn, company_manager,
                  company_address, company_tel, company_link,
-                 sro_belongs, member_status):
+                 sro_belongs, member_status, updated_at):
         self.company_name = company_name
         self.company_inn = company_inn
         self.company_manager = company_manager
@@ -31,7 +34,9 @@ class Companies(Base):
         self.company_link = company_link
         self.sro_belongs = sro_belongs
         self.member_status = member_status
+        self.updated_at = updated_at
 
 
 # Database initialize
 Base.metadata.create_all(engine)
+
--- parser.py
+++ parser.py
@@ -1,8 +1,11 @@
 import math
+import re
+import datetime
 
 import requests
 from bs4 import BeautifulSoup
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql.expression import exists
 
 from models import Companies, engine
 
@@ -14,14 +17,6 @@ session = Session()
 # http://reestr.nostroy.ru/clients/344
 # 344 is short number
 SRO_short_numbers_to_parse = [344, ]
-
-
-# def get_companies_short_numbers(short_sro_number):
-#     link_to_companies_list = \
-#         'http://reestr.nostroy.ru/reestr/clients/' + str(short_sro_number) + \
-#         '/members'
-#     r = requests.get(link_to_companies_list)
-#     data = r.text
 
 
 def get_pages_count_to_parse(short_sro_number):
@@ -38,10 +33,51 @@ def get_pages_count_to_parse(short_sro_number):
                                     {'class': 'tatal-count-wrapper'}).text)
     element_start = companies_count_raw.find('из ') + 3
     companies_count = int(companies_count_raw[element_start:])
-    pages_count = math.ceil(companies_count/20)
+    pages_count = int(math.ceil(companies_count/20))
 
     return pages_count
 
 
+def get_companies_short_links(short_sro_number):
+    pages_count = get_pages_count_to_parse(short_sro_number)
+
+    for page in range(1, pages_count + 1):
+        link_to_companies_page = 'http://reestr.nopriz.ru/reestr/clients/' \
+                                  + str(short_sro_number) + \
+                                 '/members?sort=m.registryRegistrationDate&' \
+                                 'direction=desc&page=' \
+                                  + str(page)
+        r = requests.get(link_to_companies_page)
+        data = r.text
+
+        soup = BeautifulSoup(data, features="html.parser")
+        companies_on_page = soup.findAll('a', {'href': re.compile(
+                                         '/reestr/clients/'
+                                         + str(short_sro_number)
+                                         + '/members/')})
+
+        for company in companies_on_page:
+            full_link = 'http://reestr.nopriz.ru' + str(company.get('href'))
+
+            # check that company in database:
+            c_exists = session.query(
+                   exists().where(Companies.company_link == full_link)).scalar()
+            if c_exists:
+                continue
+            else:
+            # Create new database object
+                c = Companies(company_name='null',
+                              company_inn='null',
+                              company_manager='null',
+                              company_address='null',
+                              company_tel='null',
+                              company_link=full_link,
+                              sro_belongs='null',
+                              member_status='null',
+                              updated_at=datetime.datetime.now())
+                session.add(c)
+        session.commit()
+
+
 if __name__ == '__main__':
-    print(get_pages_count_to_parse(344))
+    print(get_companies_short_links(344))
