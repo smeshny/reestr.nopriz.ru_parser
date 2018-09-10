eb7301ccd3b4e3e505923d41a33313bf560ebacd
smeshny <smeshny@gmail.com>
2018-09-10 19:23:26 +0300

    Created sro_law_violation_checker.py for check that 'is some new SRO have problems'

---
+++ sro_law_violation_checker.py
@@ -0,0 +1,46 @@
+import requests
+import smtplib
+import time
+import datetime
+
+from bs4 import BeautifulSoup
+
+from config.config import GMAIL_USER, GMAIL_PASSWORD, RECIPIENTS
+
+
+def get_page_violation_block_content():
+    r = requests.get('http://nopriz.ru/ndocs/narusheniya_sro/')
+    data = r.text
+    # get page block:
+    soup = BeautifulSoup(data, features="html.parser")
+    dump = soup.find_all('div', {'class': 'info-name'})
+    page_content = ''
+    for d in dump:
+        page_content += d.next_element.next_element.next_element + '\n'
+        page_content += d.next_sibling.next_sibling.next_element.\
+                        next_element.next_element.next_element.next_element \
+                        + '\n'
+
+    return page_content
+
+
+def send_email(msg):
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.ehlo()
+    server.starttls()
+    server.login(GMAIL_USER, GMAIL_PASSWORD)
+    server.sendmail(GMAIL_USER,
+                    RECIPIENTS,
+                    ('Subject: Оповещение о нарушениях в проектных СРО'
+                    '\r\n%s' % msg).encode('utf-8'))
+    server.quit()
+
+
+if __name__ == '__main__':
+    while 1:
+        data = get_page_violation_block_content()
+        if data != get_page_violation_block_content():
+            send_email(get_page_violation_block_content())
+        else:
+            print(f'sleeping from {datetime.datetime.now()}')
+            time.sleep(3600)
